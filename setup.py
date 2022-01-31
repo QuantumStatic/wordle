c92ad4a3721a3a1454fd3a22723e55fd14dd108f
QuantumStatic <utkarshworkemail@gmail.com>
2022-01-31 21:27:15 +0800

    testing

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 import subprocess
 
 if __name__ == "__main__":
-    p1 = subprocess.run(["brew", "install", "enchant"],
-                        stderr=subprocess.PIPE, text=True)
-    if p1.stderr.find("error") >= 0:
-        print(p1.stderr)
-        raise Exception("Failed to install enchant")
+    # p1 = subprocess.run(["brew", "install", "enchant"],
+    #                     stderr=subprocess.PIPE, text=True)
+    # if p1.stderr.find("error") >= 0:
+    #     print(p1.stderr)
+    # raise Exception("Failed to install enchant")
     setup()
